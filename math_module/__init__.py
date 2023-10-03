f50a7380cac0d6f82c2da6f65cd209407d83f114
djawad <djawad.main@gmail.com>
2023-10-03 18:37:31 +0330

    add setup.py to project

---
+++ math_module/__init__.py
@@ -0,0 +1,3 @@
+from . import math_functions
+
+__all__ = ['math_functions']
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='djawad_math',
+    version='0.0.1',
+    install_requires=[
+        'importlib-metadata; python_version == "3.8"'
+    ],
+    license='MIT'
+)
