ea71a7a7d02a0eb4580d026cc8147c77f27e7796
djawad <djawad.main@gmail.com>
2023-10-03 20:01:01 +0330

    bug

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@ from setuptools import setup
 setup(
     name='djawad_math',
     version='0.0.3',
-    install_requires=[
-        'importlib-metadata; python_version >= "3.8"'
-    ],
     license='MIT'
 )
